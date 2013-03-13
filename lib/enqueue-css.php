3a877343997c4370e33480d37972ef80f84f499c
Fernando Montoya <montogeek@gmail.com>
2013-03-12 23:39:42 -0500

    Source Sans Pro para los encabezados y Lato para la pagina

--- lib/enqueue-css.php
+++ lib/enqueue-css.php
@@ -14,9 +14,15 @@ function reverie_css_style()
 	// Register the main style under root directory
 	wp_register_style( 'reverie-stylesheet', get_stylesheet_directory_uri() . '/style.css', array(), '', 'all' );
 	
+	// Google Web Fonts - Open Sans
+	wp_register_style('source-sans', 'http://fonts.googleapis.com/css?family=Source+Sans+Pro');
+	wp_register_style('varela', 'http://fonts.googleapis.com/css?family=Varela');
+
 	wp_enqueue_style( 'reverie-normalize-stylesheet' );
 	wp_enqueue_style( 'reverie-foundation-stylesheet' );
 	wp_enqueue_style( 'reverie-stylesheet' );
+	wp_enqueue_style( 'source-sans' );
+	wp_enqueue_style( 'varela' );
 	
 }
 add_action( 'wp_enqueue_scripts', 'reverie_css_style' );
