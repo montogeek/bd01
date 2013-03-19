f48a96e31eef5f30e6d0dec7d71f3731cfb03ef5
Fernando Montoya <montogeek@gmail.com>
2013-03-18 21:48:12 -0500

    Added author.php file, changed favicon

---
+++ author.php
@@ -0,0 +1,37 @@
+<?php get_header(); ?>
+
+<!-- Row for main content area -->
+	<div class="small-12 large-9 columns" role="main">
+	
+	<?php if ( have_posts() ) : ?>
+		<div id="author-info">
+			<div id="author-avatar">
+				<?php echo get_avatar( get_the_author_meta( 'user_email' ), apply_filters( 'twentyeleven_author_bio_avatar_size', 60 ) ); ?>
+			</div><!-- #author-avatar -->
+			<div id="author-description">
+				<h2><?php printf( __( 'About %s', 'twentyeleven' ), get_the_author() ); ?></h2>
+				<?php the_author_meta( 'description' ); ?>
+			</div><!-- #author-description	-->
+		</div><!-- #author-info -->
+		<?php /* Start the Loop */ ?>
+		<?php while ( have_posts() ) : the_post(); ?>
+			<?php get_template_part( 'content', get_post_format() ); ?>
+		<?php endwhile; ?>
+		
+		<?php else : ?>
+			<?php get_template_part( 'content', 'none' ); ?>
+		
+	<?php endif; // end have_posts() check ?>
+	
+	<?php /* Display navigation to next/previous pages when applicable */ ?>
+	<?php if ( function_exists('reverie_pagination') ) { reverie_pagination(); } else if ( is_paged() ) { ?>
+		<nav id="post-nav">
+			<div class="post-previous"><?php next_posts_link( __( '&larr; Older posts', 'reverie' ) ); ?></div>
+			<div class="post-next"><?php previous_posts_link( __( 'Newer posts &rarr;', 'reverie' ) ); ?></div>
+		</nav>
+	<?php } ?>
+
+	</div>
+	<?php get_sidebar(); ?>
+		
+<?php get_footer(); ?>
